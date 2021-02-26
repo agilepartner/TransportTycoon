34a2d64e2e61fd33f8becdcecf03ca8dbbb5f9d6
Damien Hick <damien.hick@agilepartner.net>
2021-02-26 15:11:14 +0100

    wip

--- TransportTycoon/Solution.cs
+++ TransportTycoon/Solution.cs
@@ -4,11 +4,21 @@ using System;
 
 namespace TransportTycoon
 {
+    public class Vehicle
+    {
+        public int TotalDuration { get; set }
+
+        public Vehicle(int totalDuration)
+        {
+            this.TotalDuration = totalDuration;
+        }
+    }
+
     public class Solution
     {
         Dictionary<string, int> Routes = new Dictionary<string, int>()
         {
-            {"B", 5}
+            {"B", 10}
         };
 
         Dictionary<string, int> Trucks = new Dictionary<string, int>()
@@ -30,12 +40,31 @@ namespace TransportTycoon
         {
             for (int i = 0; i < destinations.Count(); i = i + 2)
             {
-                (int, int) truck1 = Transport(destinations[i]);
-                (int, int) truck2 = Transport(destinations[i + 1]);
+                var truckA = new Vehicle(Routes[destinations[i]]);
+                var truckB = new Vehicle(Routes[destinations[i]]);
 
+                while (true)
+                {
+                    this.CurrentTime++;
 
-                object p = Min(truck1.Item1, truck2.Item1);
+                    truckA.TotalDuration--;
+                    truckB.TotalDuration--;
+
+                    if(truckA.TotalDuration == 0 || truckB.TotalDuration == 0)
+                    {
+                        break;
+                    }
+                }
             }
+
+            //for (int i = 0; i < destinations.Count(); i = i + 2)
+            //{
+            //    (int, int) truck1 = Transport(destinations[i]);
+            //    (int, int) truck2 = Transport(destinations[i + 1]);
+
+
+            //    object p = Min(truck1.Item1, truck2.Item1);
+            //}
             CurrentTime = 5;
 
             if (destinations.Count() == 3)
